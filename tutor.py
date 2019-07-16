107c3f8f9b0e69f2d732d753ed989ecff9f3232c
GhostAnonH2A <49526865+GhostAnonH2A@users.noreply.github.com>
2019-07-15 23:03:38 -0500

    Add files via upload

---
+++ tutor.py
@@ -0,0 +1,20 @@
+####################################
+q = '=============================='
+w = '|| By    |--> Ghost@_AnonH2A||'
+e = '|| Thank |--> To All        ||'
+r = '=============================='
+t = '||    ||Tutor Install||     ||'
+y = '=============================='
+u = 'https://github.com/GhostAnonH2A/s-dork.git'
+print(q) ; print(w) ; print(e) ; print(r) ; print(t) ; print(y) ; print(u) ;
+####################################
+qw= '|< Linux >|' ; rt= 'apt-get install python ' ; rw='cd s-dork' ; gg='python vws.py' ;
+print(qw)
+print(rt)
+print(rw)
+print(gg)
+ppk='|< Termux >|'
+print(ppk)
+kwkw='pkg install python2\ncd s-dork\npython2 vws.py'
+print(kwkw)
+###################################
